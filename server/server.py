6157f4af8740b9d0c6b3a714ebb604193a31aa9c
ksim2-occ <ksim2@student.cccd.edu>
2021-03-23 00:47:11 -0700

    Pep8 formatting based on http://pep8online.com

--- server/server.py
+++ server/server.py
@@ -1,11 +1,9 @@
-from flask import Flask, render_template, request, jsonify   
-from flask_cors import CORS   
-
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 CORS(app, resources={r'/*': {'origins': '*'}})
-
 
 
 def welcome_message() -> str:
@@ -15,11 +13,11 @@ def welcome_message() -> str:
     Written by Adam, Kevin, Nathan, Van
     {'*'*40}
     The feature is returning a basic api request to /ping.
-    Run the vue app in client with "npm run serve" and go to localhost:8080/ping.
-    If it is successful, the client should display pong. 
+    Run the vue app in client with "npm run
+    serve" and go to localhost:8080/ping.
+    If it is successful, the client should display pong.
     If not, then nothing will be displayed.
     """
-
 
 
 @app.route('/ping', methods=['GET'])
@@ -27,8 +25,6 @@ def ping_pong():
     return jsonify('pong!')
 
 
-
 if __name__ == "__main__":
     print(welcome_message())
     app.run(debug=True)
-
